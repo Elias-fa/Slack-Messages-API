498eea4b898f51b413bf6c335da0f0b1e4f7549f
Cris Joya <MJoya@mac.attlocal.net>
2016-08-31 18:40:41 -0500

    Add Image To User And Guest Nav

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to team_directory
+      redirect_to team_directory_path
     else
       render :new
     end
